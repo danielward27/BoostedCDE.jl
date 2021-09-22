31d4007a3ad469f5d83a483543767f6fa145f776
danielward27 <danielward27@outlook.com>
2021-09-22 12:40:50 +0100

    Change mvn_loss to work with vector

--- src/losses.jl
+++ src/losses.jl
@@ -3,17 +3,17 @@
 Negative log-probability of x using vector of parameters ϕ to parameterise the
 means and cholesky decomposition of the normal distribution.
 """
-function mvn_loss(ϕ::Vector{ParamTuple}, x::Matrix{Float64}, reduction=mean)  # TODO This is probably pretty slow? 
+function mvn_loss(ϕ::Vector{<: ParamTuple}, x::Matrix{Float64}, agg = mean)
     length(ϕ) == size(x, 1) || throw(ArgumentError("length(ϕ) should match size(x,1)"))
     N = size(x, 1)
     l = Vector{Float64}(undef, N)
     for i in 1:N
-        Σ = Symmetric(ϕ[i].L*ϕ[i].L')
-        l[i] = logpdf(MvNormal(ϕ[i].μ, Σ), x[i, :])
+        μ, U = ϕ[i]
+        U = vec_to_triangular(U)
+        Λ = Symmetric(U'U)
+        h = Λ*μ
+        d = MvNormalCanon(h, Λ)
+        l[i] = logpdf(d, x[i, :])
     end
-    return reduction(-l)
+    return agg(-l)
 end
-
-
-
-
