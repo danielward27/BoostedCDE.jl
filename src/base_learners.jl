b65bcea37f2ea4d329036ce296f502150a366dac
danielward27 <danielward27@outlook.com>
2021-09-25 09:58:05 +0100

    Remove deprecated ConstantBaseLearner

--- src/base_learners.jl
+++ src/base_learners.jl
@@ -23,22 +23,3 @@ function predict(base_learner::PolyBaseLearner, θ::AbstractVector)
     poly_θ = hcat(poly_θ...)
     return poly_θ * base_learner.β
 end
-
-
-"""
-Base learner used at initialization to predict a constant value. Additional
-arguments can be provided to fit! for consistency with other base learners but
-these are ignored.
-"""
-struct ConstBaseLearner{T<:Tuple{Vararg{AbstractVector{Real}}}} <: BaseLearner
-    "The constant value to be returned."
-    ϕ::T
-end
-
-function fit!(base_learner::ConstBaseLearner,  _::AbstractVector{Real}, _::AbstractVector{Real})
-    return base_learner
-end
-
-function predict(base_learner::ConstBaseLearner, θ::AbstractMatrix{Real})
-    return fill(base_learner.ϕ, size(θ, 1))
-end
