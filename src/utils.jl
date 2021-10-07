7e71af2d11c12afe747a145df92aa5e346e73a63
danielward27 <danielward27@outlook.com>
2021-10-07 18:06:20 +0100

    Add StandardScaler

--- src/utils.jl
+++ src/utils.jl
@@ -7,4 +7,28 @@ function tri_n_el_to_d(n_el::Int64)
     return (-1 + sqrt_term) ÷ 2
 end
 
+"""
+Create a scaler, fitted to matrix x. Can be used to apply (x - μ)/σ to each
+column of a matrix.
+"""
+mutable struct StandardScaler
+    μ::Vector{Float64}
+    σ::Vector{Float64}
+    StandardScaler(x::AbstractMatrix) = new(mean.(eachcol(x)), std.(eachcol(x)))
+end
 
+"""
+Transform a matrix using the scaler
+"""
+function scale(scaler::StandardScaler, x::AbstractMatrix)
+    @unpack μ, σ = scaler
+    (x .- μ') ./ σ'
+end
+
+"""
+Unscale the matrix.
+"""
+function unscale(scaler::StandardScaler, x::AbstractMatrix)
+    @unpack μ, σ = scaler
+    x.*σ' .+ μ'
+end
--- test/utils.jl
+++ test/utils.jl
@@ -1,3 +1,17 @@
 using Test
 using BoostedCDE
+using StatsBase: mean, std
+
 @test BoostedCDE.tri_n_el_to_d.([3,6,10,15]) == [2,3,4,5]
+
+@testset "StandardScaler" begin
+    x = [2 4; 4 8; 6 12]
+    scaler = StandardScaler(x)
+    scaler.μ ≈ [4, 8]
+    scaler.σ ≈ [2, 4]
+    x_scaled = scale(scaler, x)
+    x_unscaled = unscale(scaler, x_scaled)
+    @test x ≈ x_unscaled
+    @test mean.(eachcol(x_scaled)) ≈ [0,0]
+    @test std.(eachcol(x_scaled)) ≈ [1,1]
+end
